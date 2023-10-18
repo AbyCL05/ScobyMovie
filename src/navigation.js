b38c4230d80be2dcb6a510f26977262b70f7f0ee
Abigail Chávez <chavezlozanoabigail@gmail.com>
2023-10-17 20:44:23 -0500

    PlatziMovies v1

--- src/navigation.js
+++ src/navigation.js
@@ -1,144 +1,114 @@
-searchFormBtn.addEventListener('click', () => {
-  location.hash = '#search=' + searchFormInput.value;
-});
+searchFormBtn.addEventListener('click', () => location.hash = '#search=' + searchFormInput.value)
+trendingBtn.addEventListener('click', () => location.hash = '#trends=')
+headerArrow.addEventListener('click', () => history.back())
 
-trendingBtn.addEventListener('click', () => {
-  location.hash = '#trends';
-});
+window.addEventListener('DOMContentLoaded', navigator, false )
+window.addEventListener('hashchange', navigator, false )
 
-arrowBtn.addEventListener('click', () => {
-  history.back();
-  // location.hash = '#home';
-});
-
-window.addEventListener('DOMContentLoaded', navigator, false);
-window.addEventListener('hashchange', navigator, false);
-
-function navigator() {
-  console.log({ location });
-  
-  if (location.hash.startsWith('#trends')) {
-    trendsPage();
-  } else if (location.hash.startsWith('#search=')) {
-    searchPage();
-  } else if (location.hash.startsWith('#movie=')) {
-    movieDetailsPage();
-  } else if (location.hash.startsWith('#category=')) {
-    categoriesPage();
-  } else {
-    homePage();
-  }
-
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+function navigator () {
+    if(location.hash.startsWith('#trends')) {
+        trendsPage()
+    } else if (location.hash.startsWith('#search=')) {
+        searchPage()
+    } else if (location.hash.startsWith('#movie=')) {
+        movieDetailsPage()
+    } else if (location.hash.startsWith('#category=')) {
+        categoriesPage()
+    } else {
+        homePage()
+    }
+    
+    window.scrollTo(0, 0) //O tmb:
+    //document.body.scrollTop = 0;
+    //document.documentElement.scrollTop
 }
+function homePage () {
+    headerSection.classList.remove('header-container--long')
+    headerSection.style.background = ' ';
+    headerArrow.classList.add('inactive');
+    headerArrow.classList.remove('header-arrow--white')
+    headerTitle.classList.remove('inactive')
+    headerCategoryTitle.classList.add('inactive')
+    searchForm.classList.remove('inactive')
+    trendingPreviewSection.classList.remove('inactive')
+    categoriesPreviewSection.classList.remove('inactive')
+    genericListSection.classList.add('inactive')
+    movieDetailSection.classList.add('inactive')
 
-function homePage() {
-  console.log('Home!!');
+    getTrendingMoviesPreview()
+    getCategoriesMoviesPreview()
+} 
+function categoriesPage () {
 
-  headerSection.classList.remove('header-container--long');
-  headerSection.style.background = '';
-  arrowBtn.classList.add('inactive');
-  arrowBtn.classList.remove('header-arrow--white');
-  headerTitle.classList.remove('inactive');
-  headerCategoryTitle.classList.add('inactive');
-  searchForm.classList.remove('inactive');
+    headerSection.classList.remove('header-container--long')
+    headerSection.style.background = '';
+    headerArrow.classList.remove('inactive');
+    headerArrow.classList.remove('header-arrow--white')
+    headerTitle.classList.add('inactive')
+    headerCategoryTitle.classList.remove('inactive')
+    searchForm.classList.add('inactive')
 
-  trendingPreviewSection.classList.remove('inactive');
-  categoriesPreviewSection.classList.remove('inactive');
-  genericSection.classList.add('inactive');
-  movieDetailSection.classList.add('inactive');
-  
-  getTrendingMoviesPreview();
-  getCategegoriesPreview();
+    trendingPreviewSection.classList.add('inactive')
+    categoriesPreviewSection.classList.add('inactive')
+    genericListSection.classList.remove('inactive')
+    movieDetailSection.classList.add('inactive')
+
+    //Utilizamos desestructuración para filtrar por categorias
+    const [_, categoryData] = location.hash.split('=') //Separamos el string del hash por medio del =. 
+    const [categoryId, categoryName] = categoryData.split('-') //Volvemos a dividir categoryData para obtener el ID. 
+
+    headerCategoryTitle.innerHTML = decodeURI(categoryName); //Modificamos el título de acuerdo a la categoria (category.name) 
+    getMoviesByCategory(categoryId)
 }
+function movieDetailsPage () {
+    headerSection.classList.add('header-container--long')
+    //headerSection.style.background = ' ';
+    headerArrow.classList.remove('inactive');
+    headerArrow.classList.add('header-arrow--white')
+    headerTitle.classList.add('inactive')
+    headerCategoryTitle.classList.add('inactive')
+    searchForm.classList.add('inactive')
 
-function categoriesPage() {
-  console.log('categories!!');
+    trendingPreviewSection.classList.add('inactive')
+    categoriesPreviewSection.classList.add('inactive')
+    genericListSection.classList.add('inactive')
+    movieDetailSection.classList.remove('inactive')
 
-  headerSection.classList.remove('header-container--long');
-  headerSection.style.background = '';
-  arrowBtn.classList.remove('inactive');
-  arrowBtn.classList.remove('header-arrow--white');
-  headerTitle.classList.add('inactive');
-  headerCategoryTitle.classList.remove('inactive');
-  searchForm.classList.add('inactive');
-
-  trendingPreviewSection.classList.add('inactive');
-  categoriesPreviewSection.classList.add('inactive');
-  genericSection.classList.remove('inactive');
-  movieDetailSection.classList.add('inactive');
-
-  // ['#category', 'id-name']
-  const [_, categoryData] = location.hash.split('=');
-  const [categoryId, categoryName] = categoryData.split('-');
-
-  headerCategoryTitle.innerHTML = categoryName;
-  
-  getMoviesByCategory(categoryId);
+    const [_, id] = location.hash.split('=')
+    getMovieById(id)
 }
+function searchPage () {
+    headerSection.classList.remove('header-container--long')
+    headerSection.style.background = ' ';
+    headerArrow.classList.remove('inactive');
+    headerArrow.classList.remove('header-arrow--white')
+    headerTitle.classList.add('inactive')
+    headerCategoryTitle.classList.add('inactive')
+    searchForm.classList.remove('inactive')
 
-function movieDetailsPage() {
-  console.log('Movie!!');
+    trendingPreviewSection.classList.add('inactive')
+    categoriesPreviewSection.classList.add('inactive')
+    genericListSection.classList.remove('inactive')
+    movieDetailSection.classList.add('inactive')
 
-  headerSection.classList.add('header-container--long');
-  // headerSection.style.background = '';
-  arrowBtn.classList.remove('inactive');
-  arrowBtn.classList.add('header-arrow--white');
-  headerTitle.classList.add('inactive');
-  headerCategoryTitle.classList.add('inactive');
-  searchForm.classList.add('inactive');
-
-  trendingPreviewSection.classList.add('inactive');
-  categoriesPreviewSection.classList.add('inactive');
-  genericSection.classList.add('inactive');
-  movieDetailSection.classList.remove('inactive');
-
-  // ['#movie', '234567']
-  const [_, movieId] = location.hash.split('=');
-  getMovieById(movieId);
+    const [_, query] = location.hash.split('=')
+    console.log(query);
+    renderSearch(query)
 }
+function trendsPage () {
+    headerSection.classList.remove('header-container--long')
+    headerSection.style.background = '';
+    headerArrow.classList.remove('inactive');
+    headerArrow.classList.remove('header-arrow--white')
+    headerTitle.classList.add('inactive')
+    headerCategoryTitle.classList.remove('inactive')
+    searchForm.classList.add('inactive')
 
-function searchPage() {
-  console.log('Search!!');
+    trendingPreviewSection.classList.add('inactive')
+    categoriesPreviewSection.classList.add('inactive')
+    genericListSection.classList.remove('inactive')
+    movieDetailSection.classList.add('inactive')
 
-  headerSection.classList.remove('header-container--long');
-  headerSection.style.background = '';
-  arrowBtn.classList.remove('inactive');
-  arrowBtn.classList.remove('header-arrow--white');
-  headerTitle.classList.add('inactive');
-  headerCategoryTitle.classList.add('inactive');
-  searchForm.classList.remove('inactive');
-
-  trendingPreviewSection.classList.add('inactive');
-  categoriesPreviewSection.classList.add('inactive');
-  genericSection.classList.remove('inactive');
-  movieDetailSection.classList.add('inactive');
-
-  // ['#search', 'platzi']
-  const [_, query] = location.hash.split('=');
-  getMoviesBySearch(query);
+    headerCategoryTitle.innerHTML = 'Tendencias'
+    getTrendingMovies()
 }
-
-function trendsPage() {
-  console.log('TRENDS!!');
-
-  headerSection.classList.remove('header-container--long');
-  headerSection.style.background = '';
-  arrowBtn.classList.remove('inactive');
-  arrowBtn.classList.remove('header-arrow--white');
-  headerTitle.classList.add('inactive');
-  headerCategoryTitle.classList.remove('inactive');
-  searchForm.classList.add('inactive');
-
-  trendingPreviewSection.classList.add('inactive');
-  categoriesPreviewSection.classList.add('inactive');
-  genericSection.classList.remove('inactive');
-  movieDetailSection.classList.add('inactive');
-
-  headerCategoryTitle.innerHTML = 'Tendencias';
-
-  getTrendingMovies();
-}
-
-
--- src/nodes.js
+++ src/nodes.js
@@ -1,27 +1,26 @@
-// Sections
-const headerSection = document.querySelector('#header');
-const trendingPreviewSection = document.querySelector('#trendingPreview');
-const categoriesPreviewSection = document.querySelector('#categoriesPreview');
-const genericSection = document.querySelector('#genericList');
-const movieDetailSection = document.querySelector('#movieDetail');
+const selectors = (id) => document.querySelector(id)
 
-// Lists & Containers
-const searchForm = document.querySelector('#searchForm');
-const trendingMoviesPreviewList = document.querySelector('.trendingPreview-movieList');
-const categoriesPreviewList = document.querySelector('.categoriesPreview-list');
-const movieDetailCategoriesList = document.querySelector('#movieDetail .categories-list');
-const relatedMoviesContainer = document.querySelector('.relatedMovies-scrollContainer');
+//Sections
+const headerSection = selectors('#header')
+const trendingPreviewSection = selectors('#trendingPreview')
+const categoriesPreviewSection = selectors('#categoriesPreview')
+const genericListSection = selectors('#genericList')
+const movieDetailSection = selectors('#movieDetail')
 
-// Elements
-const headerTitle = document.querySelector('.header-title');
-const arrowBtn = document.querySelector('.header-arrow');
-const headerCategoryTitle = document.querySelector('.header-title--categoryView');
+//List Containers
+const searchForm = selectors('#searchForm')
+const trendingMoviesPreviewList = selectors('.trendingPreview-movieList')
+const categoriesPreviewList = selectors('.categoriesPreview-list')
+const movieDetailCategoriesList = selectors('#movieDetail .categories-list')
+const relatedMoviesContainer = selectors('.relatedMovies-scrollContainer')
 
-const searchFormInput = document.querySelector('#searchForm input');
-const searchFormBtn = document.querySelector('#searchBtn');
-
-const trendingBtn = document.querySelector('.trendingPreview-btn');
-
-const movieDetailTitle = document.querySelector('.movieDetail-title');
-const movieDetailDescription = document.querySelector('.movieDetail-description');
-const movieDetailScore = document.querySelector('.movieDetail-score');
+//Elements
+const headerTitle = selectors('.header-title')
+const headerArrow = selectors('.header-arrow')
+const headerCategoryTitle = selectors('.header-title--categoryView')
+const searchFormInput = selectors('#searchForm input')
+const searchFormBtn = selectors('#searchBtn')
+const trendingBtn = selectors('.trendingPreview-btn')
+const movieDetailTitle = selectors('.movieDetail-title')
+const movieDetailScore = selectors('.movieDetail-score')
+const movieDetailDescription = selectors('.movieDetail-description')
